5c580298b130e9947cead8959adcfefd9e207bde
Yara <148970252+cheYarina@users.noreply.github.com>
2023-12-21 13:55:59 +0200

    Update sign_up.php

--- BE/sign_up.php
+++ BE/sign_up.php
@@ -1,31 +1,51 @@
 <?php
+require_once 'dbconnect.php'; 
 
-require_once("../front end/common/menu.php"); 
-require_once 'dbconnect.php';
+class User {
+    public $username;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $password;
 
+    public function __construct($username, $firstname, $lastname, $email, $password) {
+        $this->username = $username;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function save() {
+        global $pdo; 
+        $hashed_password = password_hash($this->password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO user (username, fullname, email, password) VALUES (?, ?, ?, ?)";
+        $stmt = $pdo->prepare($query);
+        return $stmt->execute([$this->username, $this->firstname . ' ' . $this->lastname, $this->email, $hashed_password]);
+    }
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = htmlspecialchars($_POST["username"]);
-    $fullname = htmlspecialchars($_POST["firstname"]) . ' ' . htmlspecialchars($_POST["lastname"]);
+    $firstname = htmlspecialchars($_POST["firstname"]);
+    $lastname = htmlspecialchars($_POST["lastname"]);
     $email = htmlspecialchars($_POST["email"]);
     $password = htmlspecialchars($_POST["pass"]);
-    
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $confirmPassword = htmlspecialchars($_POST["confirmPassword"]);
 
-    $query = "INSERT INTO user (username, fullname, email, password) VALUES (?, ?, ?, ?)";
-
-    $stmt = $pdo->prepare($query);
-    
-   
-    if ($stmt->execute([$username, $fullname, $email, $hashed_password])) {
-        
-        session_start();
-        $_SESSION["username"] = $username; 
-        header("Location: index.php");
+    if ($password !== $confirmPassword) {
+        header("Location: sign_up.php?error=password_mismatch");
         exit;
+    }
+
+    $user = new User($username, $firstname, $lastname, $email, $password);
+
+    if ($user->save()) {
+        session_start();
+        $_SESSION["username"] = $user->username;
+        header("Location: index.php");
     } else {
         header("Location: sign_up.php?error=registration_failed");
-        exit;
     }
 } else {
     header("Location: sign_up.php");
