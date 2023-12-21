b66b76e97b7e2caf14ad308f3bcbf10885a10f31
Yara <148970252+cheYarina@users.noreply.github.com>
2023-12-21 13:07:45 +0200

    Update sign_up.php

--- BE/sign_up.php
+++ BE/sign_up.php
@@ -1,35 +1,34 @@
 <?php
 
-require_once(""); /must replace with path to database
+require_once("../front end/common/menu.php"); 
+require_once 'dbconnect.php';
 
 
-$firstname = htmlspecialchars($_POST["firstname"]);
-$lastname = htmlspecialchars($_POST["lastname"]);
-$email = htmlspecialchars($_POST["email"]);
-$password = htmlspecialchars($_POST["pass"]);
-$sex = htmlspecialchars($_POST["sex"]);
-$ageGroup = htmlspecialchars($_POST["ageGroup"]);
-$language = htmlspecialchars($_POST["language"]);
-
-
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-
-$query = "INSERT INTO tbl_users (firstname, lastname, email, password, sex, age_group, language) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-
-$stmt = $db->prepare($query);
-
-
-if ($stmt->execute([$firstname, $lastname, $email, $hashed_password, $sex, $ageGroup, $language])) {
-   
-    session_start();
-    $_SESSION["username"] = $email;
-
-   
-    header("Location: index.php");
-} else {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST["username"]);
+    $fullname = htmlspecialchars($_POST["firstname"]) . ' ' . htmlspecialchars($_POST["lastname"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["pass"]);
     
-    header("Location: signup.php?error=registration_failed");
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO user (username, fullname, email, password) VALUES (?, ?, ?, ?)";
+
+    $stmt = $pdo->prepare($query);
+    
+   
+    if ($stmt->execute([$username, $fullname, $email, $hashed_password])) {
+        
+        session_start();
+        $_SESSION["username"] = $username; 
+        header("Location: index.php");
+        exit;
+    } else {
+        header("Location: signup.php?error=registration_failed");
+        exit;
+    }
+} else {
+    header("Location: signup.php");
+    exit;
 }
 ?>
